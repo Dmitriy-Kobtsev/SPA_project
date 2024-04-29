520fe7b070ec00186d84b37c2d58721ac8e3b9ab
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2024-04-29 21:01:13 +0300

    fix .env

--- course/serializers.py
+++ course/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from course.models import Course, Lesson
 
@@ -7,6 +7,17 @@ class CourseSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = '__all__'
+
+
+class CourseDetailSerializer(ModelSerializer):
+    count_course_lessons = SerializerMethodField()
+
+    def get_count_course_lessons(self, course):
+        return Course.object.filter(lesson=course.lesson).count
+
+    class Meta:
+        model = Course
+        fields = ("name", "img", "description")
 
 
 class LessonSerializer(ModelSerializer):
--- course/views.py
+++ course/views.py
@@ -3,12 +3,16 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
 from course.models import Course, Lesson
-from course.serializers import CourseSerializer, LessonSerializer
+from course.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
-    serializer_class = CourseSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return CourseDetailSerializer
+        return CourseSerializer
 
 
 class LessonCreateAPIView(CreateAPIView):
