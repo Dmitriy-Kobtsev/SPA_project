54841d14db2cee3f4575e9cc5e1ec84122edf0f0
Dmitriy-Kobtsev <kibtsev94@gmail.com>
2024-05-03 22:51:47 +0300

    hw_2

--- course/views.py
+++ course/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-from course.models import Course, Lesson
-from course.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
+from course.models import Course, Lesson, Payments
+from course.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer, PaymentsSerializer
 
 
 class CourseViewSet(ModelViewSet):
@@ -38,3 +40,10 @@ class LessonDestroyAPIView(DestroyAPIView):
 class LessonUpdateAPIView(UpdateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
+
+class PaymentsListAPIView(ListAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_method')
+    ordering_fields = ('date_pay',)
